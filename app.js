6d54107b514bd36111cc3fd9babc5368b0058f87
zainRazzaq <zainrazzaq2003@gmail.com>
2023-04-02 16:40:27 +0500

    Converted previous progress into ejs and added the express server

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+
+app.get('/login', function(req, res) {
+    res.render('login.ejs');
+});
+
+app.get('/signup', function(req, res) {
+    res.render('signup.ejs');
+});
+
+
+
+
+
+
+
+
+
+
+app.listen('3000', function(req, res) {
+    console.log('Server is running on port 3000');
+});
