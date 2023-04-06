c40eab316083d4513bc53d2b983903c369078f24
zainRazzaq <zainrazzaq2003@gmail.com>
2023-04-06 11:30:50 +0500

    Added password encription

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const bcrypt= require('bcrypt');  // for hashing password
 
 var mysql = require('mysql');
 
@@ -14,8 +15,6 @@ connection.connect(function(err) {
     }
     console.log('Connected to MySQL as id ' + connection.threadId);
 });
-
-  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -66,14 +65,18 @@ app.post('/signup', function(req, res) {
                     res.redirect('/signup');
                 }
                 else{
-                    connection.query('INSERT INTO users (username,password,email) values(?,?,?)',[userData.name,userData.password,userData.email], function (error, results, fields) {
-                        if (error) {
-                            console.log("error ocurred for query",error);
-                            res.redirect('/signup');
-                        } else {
-                            res.redirect('/login');
-                        }
+                    bcrypt.hash(userData.password, 10, function(err, hash) {
+                        userData.password = hash;
+                        connection.query('INSERT INTO users (username,password,email) values(?,?,?)',[userData.name,userData.password,userData.email], function (error, results, fields) {
+                            if (error) {
+                                console.log("error ocurred for query",error);
+                                res.redirect('/signup');
+                            } else {
+                                res.redirect('/login');
+                            }
+                        });
                     });
+                    
                 }
             }
         });
@@ -91,12 +94,14 @@ app.post('/login', function(req, res) {
             res.redirect('/login');
         } else {
             if(results.length >0){
-                if(results[0].password == userData.password){
-                    res.redirect('/form');
-                }
-                else{
-                    res.redirect('/login');
-                }
+                bcrypt.compare(userData.password, results[0].password, function(err, result) {
+                    if(result) {
+                        res.redirect('/form');
+                    }
+                    else {
+                        res.redirect('/login');
+                    }
+                });
             }
             else{
                 res.redirect('/login');
